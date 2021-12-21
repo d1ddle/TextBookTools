d4eb804d6238ce926355c3b332ebf5e8fa7d88c5
d1ddle <69437145+d1ddle@users.noreply.github.com>
2021-12-21 14:53:45 +0000

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,11 +1,19 @@
 import requests
+
+# EDIT THESE BEFORE RUNNING TO CHANGE THE BOOK DOWNLOAD
 TOTAL_PAGES = 241
+URL_PREFIX = "https://resources.pearsonactivelearn.com/r00/r0062/r006201/r00620161/current/OPS/images/577991-"
+URL_SUFFIX = ".jpg"
+
+
+# DONT TOUCH PAST HERE UNLESS YOU KNOW WHAT YOUR DOING
+print("\nTrying for book starting at URL: " + URL_PREFIX + str(1).zfill(3) + URL_SUFFIX + "\n")
 
 for i in range(0, TOTAL_PAGES):
     print(str(i).zfill(len(str(TOTAL_PAGES))))
 
     #requesting page...
-    response = requests.get("https://resources.pearsonactivelearn.com/r00/r0062/r006201/r00620161/current/OPS/images/577991-" + str(i).zfill(3) + ".jpg", stream=True)
+    response = requests.get(URL_PREFIX + str(i).zfill(3) + URL_SUFFIX, stream=True)
 
     if not response.ok:
         #failed
@@ -13,7 +21,7 @@ for i in range(0, TOTAL_PAGES):
         
     if response.ok:
         #success, download the image
-        with open("577991-" + str(i).zfill(len(str(TOTAL_PAGES))) + ".jpg", 'wb') as handle:
+        with open(URL_PREFIX.split("/")[-1] + str(i).zfill(len(str(TOTAL_PAGES))) + URL_SUFFIX, 'wb') as handle:
             for block in response.iter_content(1024):
                 if not block:
                     break
